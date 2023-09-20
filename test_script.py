af4dd9653f1f5f84b34cb140158b2974a173c27d
VCM admin account <vcm@vcm-30918.vm.duke.edu>
2023-09-20 16:00:07 -0400

    fix for line length

--- test_script.py
+++ test_script.py
@@ -7,8 +7,6 @@ def test_script_main_logic():
     stats = get_descriptive_statistics(data)
     assert "mean" in stats.index, "Means not calculated!"
 
-    # Since plotting just shows the figure, we can potentially check for errors by running the function.
-    # But we can't assert on matplotlib figures easily without a lot of additional code.
     try:
         plot_data_distribution(data, "Price", save=False)
     except Exception as e:
